6cacb57aae4519c3d5d9396643f278f7207dc301
Cassidy Pelacek <145030622+casspb@users.noreply.github.com>
2025-03-03 14:54:21 -0500

    form attempt
    form attempt

--- includes/connect.php
+++ includes/connect.php
@@ -1,13 +1,13 @@
 <?php
 //mac connect
-$dsn = "mysql:host=localhost;port=8888;dbname=pelacek_cassidy_portfolio1;charset=utf8mb4";
+//$dsn = "mysql:host=localhost;port=8888;dbname=pelacek_cassidy_portfolio1;charset=utf8mb4";
 //windows
-//$dsn = "mysql:host=localhost;dbname=pelacek_cassidy_portfolio1;charset=utf8mb4";
+$dsn = "mysql:host=localhost;dbname=pelacek_cassidy_portfolio1;charset=utf8mb4";
 try {
     // Create a PDO connection
     // for mac it needs to be root root
-    $connection = new PDO($dsn, 'root', 'root');  
-    echo "Connected successfully!";
+    $connection = new PDO($dsn, 'root', '');  
+    //echo "Connected successfully!";
 } catch (Exception $e) {
     error_log($e->getMessage());
     exit('Unable to connect');
